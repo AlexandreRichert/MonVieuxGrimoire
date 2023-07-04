5b85a31eb9b4aae8f624b6c4a7b5e2971f30c446
Alexandre Richert <121611457+AlexandreRichert@users.noreply.github.com>
2023-07-04 21:56:33 +0200

    ajout dossier middleware contenant le fichier auth.js

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,7 +1,7 @@
 const Book = require('../models/book');
 
 exports.createBook = (req, res, next) => {
-    //delete req.body._id;
+    delete req.body._id;
     const book = new Book({
       ...req.body
     });
