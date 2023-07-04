2873747fcf6adc69bdc5f9e6e6a636e94706e990
Alexandre Richert <121611457+AlexandreRichert@users.noreply.github.com>
2023-07-04 21:49:32 +0200

    inscription et connexion possible (token généré)

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const booksRoutes = require('./routes/books');
+
+mongoose.connect('mongodb+srv://firstuser:DQ3gWJZZ7KjS1tzL@cluster0.vdthjca.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-const Book = require('./models/book');
-
-app.use(express.json());
-
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,45 +19,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/books', (req, res, next) => {
-  //delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use(express.json());
 
-app.get('/api/books/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
+app.use('/api/books', booksRoutes);
+app.use('/api/auth', userRoutes);
 
-app.put('/api/books/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books', (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-});
-
-mongoose.connect('mongodb+srv://firstuser:DQ3gWJZZ7KjS1tzL@cluster0.vdthjca.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 module.exports = app;
 
--- backend/models/book.js
+++ backend/models/book.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
+    //Schema à revoir après la connexion (vérifier si tout est bien initialisé)
+  id: { type: Number, required: true },
+  //userId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
   price: { type: Number, required: true },
-  genre: { type: String, required: true }
+  genre: { type: String, required: true },
+  grade : { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
