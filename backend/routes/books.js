e6068a67d9c2fac8175e02d94c218f135b755365
Alexandre Richert <121611457+AlexandreRichert@users.noreply.github.com>
2023-07-07 10:21:24 +0200

    suppression de l'ancien code en commentaire

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,19 +1,3 @@
-/*const express = require('express');
-const router = express.Router();
-
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
-
-const bookCtrl = require('../controllers/books');
-
-router.post('/', auth, multer, bookCtrl.createBook );
-router.get('/:id', auth, bookCtrl.getOneBook );
-router.put('/:id', auth, bookCtrl.modifyBook );
-router.delete('/:id', auth, bookCtrl.deleteBook );
-router.get('/', auth, bookCtrl.getAllBooks );
-
-
-module.exports = router;*/
 
 const express = require('express');
 const router = express.Router();
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,3 @@
-/*const express = require('express');
-const router = express.Router();
-
-const userCtrl = require('../controllers/user');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
-
-module.exports = router;*/
 
 const express = require('express');
 const router = express.Router();
